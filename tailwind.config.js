7101a0699b442dffd2de797232b616591a05f9d8
Dave Leslie <dave.leslie.online@gmail.com>
2021-08-26 18:08:27 +0200

    Updated styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#8DC63F",
+        secondary: "#1DB0DA",
+        tertiary: "#9D9D9D"
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+     
+      }
+    },
   },
   plugins: [],
 }
